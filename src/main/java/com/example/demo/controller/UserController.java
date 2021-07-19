fa7f5ad6afb297c82263acf7e2495fd3c94cb230
87463 <874631592@qq.com>
2021-07-19 15:28:08 +0800

    页面提交

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @GetMapping("toUser")
     public String toUser(){
+        System.out.println();
         return "hello";
     }
 }
